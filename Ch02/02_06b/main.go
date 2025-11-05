f2ef04dd6e2d59f882590d95fa129d51db3ba637
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-05 03:37:33 +0200

    Learning a little about DATES...

--- Ch02/02_06b/main.go
+++ Ch02/02_06b/main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	fmt.Println("--- Dates and times ---")
+	goLaunchTime := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Printf("Go Launced at %s\n", goLaunchTime)
 
-	fmt.Println("Dates and times")
+	now := time.Now()
+	fmt.Printf("The time now is %s\n", now)
+	fmt.Printf("The time now is %T\n", now)
 
+	fmt.Printf(now.Format(time.ANSIC)+ "\n")
+
+	// addDate (year, month, day)
+	tomorrow := now.AddDate(0, 0, 1)
+	fmt.Printf(tomorrow.Format(time.ANSIC)+ "\n")
+
+	// format := "Mon 2006/02-01"
+	format := "Mon 2006-02-01"
+	fmt.Printf(tomorrow.Format(format)+ "\n")
 }
