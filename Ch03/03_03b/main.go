fe282790fd34248826c75ff4f4c7a0ff6c890f9c
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-05 04:05:39 +0200

    Intro to arrays

--- Ch03/03_03b/main.go
+++ Ch03/03_03b/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	// var colors [4]string
+	// colors[0] = "Red"
+	// colors[1] = "Green"
+	// colors[2] = "Blue"
+	var colors [4]string = [4]string{"Red", "Green", "Blue"}
+	colors[3] = "Black"
+	fmt.Println("Colors:", colors)
+
+	var number = [5]int{5, 1, 2, 4}
+	fmt.Println("Numbers:", number)
+	fmt.Println("len(Numbers):", len(number))
+	fmt.Println("len(Colors):", len(colors))
 }
