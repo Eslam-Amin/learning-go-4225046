599fe1015d434236e689f4bf280a12dd59b6be8d
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-05 03:31:19 +0200

    Math package

--- Ch02/02_05b/main.go
+++ Ch02/02_05b/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -9,5 +10,7 @@ func main() {
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	sum := f1 + f2 + f3
 	fmt.Println("Float sum:", sum)
-
+	sum = math.Round(sum * 100)/100
+	fmt.Println("The sum after math.round:", sum)
+	fmt.Printf("The value of PI math.Pi: %.2f\n", math.Pi)
 }
