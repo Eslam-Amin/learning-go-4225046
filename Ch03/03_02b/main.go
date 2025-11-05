4618951ba942778f6d149ab906fd3d5ebd18a376
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-05 04:01:14 +0200

    Playing with pointers

--- Ch03/03_02b/main.go
+++ Ch03/03_02b/main.go
@@ -5,5 +5,23 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	var p *int = &anInt
+
+	if p == nil {
+		fmt.Println("p is nil")
+	}else{
+		fmt.Println("the value of p is",*p)
+	}
+	
+	
+	fValue1 := 42.13
+	pointerOfValue1 := &fValue1
+	fmt.Println("the value of pointerOfValue1 is",*pointerOfValue1)
+	
+	*pointerOfValue1 = *pointerOfValue1 / 31
+	fmt.Println("the value of pointerOfValue1 is",*pointerOfValue1)
+	fmt.Println("the value of fValue1 is",fValue1)
+	
 }
+
