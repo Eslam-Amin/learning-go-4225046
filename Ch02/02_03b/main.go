76b3dc80f163d68b45f7f592dda0df5ca157f50c
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-05 03:16:19 +0200

    Playing with input and output strings

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n')
+	fmt.Println(name)
+	fmt.Printf("type of str %T\n", name)
+	fmt.Println("what's your age, in years?", name)
+	age, _:=reader.ReadString('\n')
+	fmt.Printf("type of age %T\n", age)
+	ageAsNumber,  err := strconv.ParseFloat(strings.TrimSpace(age),64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("your age is", ageAsNumber)
+		fmt.Printf("type of ageAsNumber %T\n", ageAsNumber)
+	}
 }
