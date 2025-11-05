180b8f3358f68d064854ce61c252dc6f1a9eee48
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-05 13:54:55 +0200

    Dealing with multiple params

--- Ch05/05_01b/main.go
+++ Ch05/05_01b/main.go
@@ -5,5 +5,38 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomething()
 }
+
+func doSomething() {
+	fmt.Println("Doing something")
+	value1 := 5
+	value2 := 10
+	value3 := 56
+	values := make([]int, 0, 5)
+	values = []int{12,15,16,22,90}
+	sum, count, average := addAllValues(values...)
+	fmt.Printf("The sum is %v.\n", sum)
+	fmt.Printf("The count is %v.\n", count)
+	fmt.Printf("The average is %v.\n", average)
+	
+	sum, count, average = addAllValues(value1, value2, value3, 125)
+	fmt.Printf("The sum is %v.\n", sum)
+	fmt.Printf("The count is %v.\n", count)
+	fmt.Printf("The average is %v.\n", average)
+
+}
+
+func addValues(value1, value2 int) int {
+	return value1 + value2
+}
+
+	func addAllValues(values ...int) (int, int, float64) {
+		sum := 0
+		for _, v := range values {
+			sum += v
+		}
+		count := len(values)
+		average := float64(sum) / float64(count)
+		return sum, count, average
+	}
